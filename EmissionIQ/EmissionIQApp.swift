ce0878076fb5666eb2f338c7d8819f0aa0cbc8e3
matt54633 <matt54633@gmail.com>
2024-01-30 16:13:36 +0000

    Initial Commit

---
+++ EmissionIQ/EmissionIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmissionIQApp.swift
+//  EmissionIQ
+//
+//  Created by Matt Sullivan on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmissionIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
