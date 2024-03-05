0b571c95bc64fba0ee1cd61293d96a19bb03267c
matt54633 <matt54633@gmail.com>
2024-03-05 23:22:27 +0000

    Add TipKit Tips & Rename Folders

---
+++ EmissionIQ/Tips/StatsOverviewTip.swift
@@ -0,0 +1,26 @@
+//
+//  StatsOverviewTip.swift
+//  EmissionIQ
+//
+//  Created by Matt Sullivan on 05/03/2024.
+//
+
+import Foundation
+import TipKit
+
+// Tip to explain the stats pages
+struct StatsOverviewTip: Tip {
+    
+    var title: Text {
+        Text("Swipe for more")
+    }
+    
+    var message: Text? {
+        Text("Swipe to see detailed breakdowns of more stats!")
+    }
+    
+    var options: [Option] {
+        MaxDisplayCount(1)
+    }
+    
+}
